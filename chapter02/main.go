d3767eb3f979473c0e340867e3b51a7e5c36ae45
jamesvrooney <jamesvrooney@hotmail.com>
2021-02-10 21:50:11 +0000

    Added contact page.

--- chapter02/main.go
+++ chapter02/main.go
@@ -7,7 +7,12 @@ import (
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Hello James, you big eejit! Up Leitrim</h1>")
+
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello James, you big eejit! Up Leitrim</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch email: <a href=\"mailto:jamesvrooney@hotmail.com\">jamesvrooney@hotmail.com</a>")
+	}
 }
 
 func main() {
