e5ba3db35e03c8fd8f80a47e0b465eaa88073ca0
jamesvrooney <jamesvrooney@hotmail.com>
2021-02-10 21:14:29 +0000

    Added gitignore file

--- chapter02/main.go
+++ chapter02/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Print("Another request")
+	fmt.Fprint(w, "<h1>Hello James, you big eejit! Up Leitrim</h1>")
+}
+
 func main() {
-	println("My name is James")
+	http.HandleFunc("/", helloHandler)
+
+	http.ListenAndServe(":3000", nil)
 }
